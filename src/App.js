f2f482a6ed38e9008313a99b2d446f09f261b735
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2020-10-12 21:08:40 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import uuid from 'react-uuid';
 
 
 
-// const initialExpense = [
-//   { id: uuid(), charge: "rent", amount: 1500 },
-//   { id: uuid(), charge: "car", amount: 2500 },
-//   { id: uuid(), charge: "credit bill", amount: 1200 }
-// ];
 const initialExpense = localStorage.getItem('expenses') ? JSON.parse(localStorage.getItem('expenses')) : []
 
 
@@ -24,15 +19,15 @@ function App() {
   const [edit, setEdit] = useState(false);
   const [id, setId] = useState(0);
 
-  useEffect(() => {
-    localStorage.setItem('expenses', JSON.stringify(expenses));
-  }, [expenses]);
+  useEffect(()=>{
+    localStorage.setItem('expenses', JSON.stringify(expenses)); 
+  },[expenses]);
 
-  const handleAmount = e => {
-    setAmount(e.target.value)
-  }
   const handleCharge = e => {
     setCharge(e.target.value)
+  }
+  const handleAmount = e => {
+    setAmount(e.target.value)
   }
 
   const handleAlert = ({ type, text }) => {
@@ -48,19 +43,21 @@ function App() {
     if (charge !== '' && amount > 0) {
       if (edit) {
         let tempExpenses = expenses.map(expense => {
-          return expense.id === id ? { ...expense, charge, amount } : expense;
+           return expense.id === id ? {...expense, charge, amount }  : expense;
         });
         setExpenses(tempExpenses);
         handleAlert({ type: 'success', text: 'item Edited' });
         setEdit(false);
-      } else {
+      }
+      else {
         const expense = { id: uuid(), charge, amount };
         setExpenses([...expenses, expense]);
         handleAlert({ type: 'success', text: 'item added' });
       }
       setCharge('');
       setAmount('');
-    } else {
+    }
+    else {
       handleAlert({ type: 'danger', text: `charge can't be empty and amount has to greater than zero` })
     }
 
@@ -87,41 +84,30 @@ function App() {
 
   }
 
-  return ( <
-        >
-    {
-      alert.show && < Alert type={alert.type}
-        text={alert.text}
-      />} <
-            h1 > budget calculator < /h1> <
-            main className="App" >
-        <
-          ExpenseForm charge={charge}
+  return (
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm charge={charge}
           amount={amount}
           handleAmount={handleAmount}
           handleCharge={handleCharge}
           handleSubmit={handleSubmit}
           edit={edit}
-        /> <
-          ExpenseList expenses={expenses}
-          handleDelete={handleDelete}
-          handleEdit={handleEdit}
-          clearItems={clearItems}
-        /> < /
-            main > <
-            h1 >
-          total spending: < span className="total" >
-            $ {
-              expenses.reduce((acc, curr) => {
-                return acc += parseInt(curr.amount);
-              }, 0)
-            } <
-                /span> < /
-            h1 >
+        />
+        <ExpenseList expenses={expenses} handleDelete={handleDelete} handleEdit={handleEdit} clearItems={clearItems} />
+      </main>
+      <h1>
+        total  spending: <span className="total">
+          $ {expenses.reduce((acc, curr) => {
+        return acc += parseInt(curr.amount);
+      }, 0)}
+        </span>
+      </h1>
 
-            <
-            />
-        );
-    }
+    </>
+  );
+}
 
-    export default App;
+export default App;
